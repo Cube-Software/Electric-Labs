16f7d87137d80213047c69c0417a36a5519a7417
KacperKostka08 <32651459+kacperks@users.noreply.github.com>
2021-08-24 00:28:02 +0200

    Maths Update

--- Electric-Labs/Source/maths/maths.cpp
+++ Electric-Labs/Source/maths/maths.cpp
@@ -1,1 +1,69 @@
 #include "maths.hpp"
+
+namespace EL::maths {
+    float Sqrt(float num) { float p = 0.000001; float s = num; while ((s - num / s) > p) { s = (s + num / s) / 2; } return s; }
+
+	float Q_rsqrt(float num) { 
+        long i;
+        float x2, y;
+        const float threehalfs = 1.5F;
+
+        x2 = num * 0.5F;
+        y  = num;
+        i  = * ( long * ) &y;                       
+        i  = 0x5f3759df - ( i >> 1 );               
+        y  = * ( float * ) &i;
+        y  = y * ( threehalfs - ( x2 * y * y ) );  
+
+        return y; 
+    }
+
+	float Sin(int deg) {
+		deg %= 360;
+		float rad = deg * PI / 180;
+		float sin = 0;
+		for (int i = 0; i < 7; i++) {
+			sin += power(-1, i) * power(rad, 2 * i + 1) / fact(2 * i + 1);
+		}
+		return sin;
+	}
+
+	float Cos(int deg) {
+		deg %= 360;
+		float rad = deg * PI / 180;
+		float cos = 0;
+
+		int i;
+		for (i = 0; i < 7; i++) {
+			cos += power(-1, i) * power(rad, 2 * i) / fact(2 * i);
+		}
+		return cos;
+	}
+
+	float Tan(int deg) { return Sin(deg) / Cos(deg); }
+
+	float power(float base, int exp) {
+		if (exp < 0) {
+			if (base == 0)
+				return -0;
+			return 1 / (base * power(base, (-exp) - 1));
+		}
+		if (exp == 0)
+			return 1;
+		if (exp == 1)
+			return base;
+		return base * power(base, exp - 1);
+	}
+
+	int fact(int n) {
+		return n <= 0 ? 1 : n * fact(n - 1);
+	}
+
+	float abs(float num) {
+		return (num < 0.0f) ? -num : num;
+	}
+
+	float abs(int num) {
+		return (num < 0) ? (float)(-num) : (float)(num);
+	}
+}
--- Electric-Labs/Source/maths/maths.hpp
+++ Electric-Labs/Source/maths/maths.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-#define MATH_PI 3.1415926535897932384626433832795
-#define ToRadians(x) (float)(((x) * MATH_PI / 180.0f))
-#define ToDegrees(x) (float)(((x) * 180.0f / MATH_PI))
+#define PI 3.1415926535897932384626433832795
+#define Radians(x) (float)(((x) * MATH_PI / 180.0f))
+#define Degrees(x) (float)(((x) * 180.0f / MATH_PI))
 
+// Basic stuff
 namespace EL::Maths{
 	static float Cos(int deg);
 	static float Sin(int deg);
@@ -15,4 +16,66 @@ namespace EL::Maths{
 	static float abs(float num);
 	static float abs(int num);
 	static float Lerp(float start, float stop, float step);
+}
+
+// Vectors
+
+namespace EL::Maths{
+    class Vector2 {
+
+    public:
+
+        float x;
+        float y;
+
+        inline Vector2() {
+            x = 0;
+            y = 0;
+        }
+
+        inline Vector2(float p_x, float p_y) {
+            x = p_x;
+            y = p_y;
+        }
+
+        inline float Length() const {
+            return Sqrt(x * x + y * y);
+        }
+
+        inline Vector2 Middle(Vector2 p_vector) const {
+            return Vector2(abs(p_vector.x - x), abs(p_vector.y - y));
+        }
+
+        Vector2 Normalized() const {
+			return *this * Q_rsqrt(Length());
+		}
+
+        Vector2 operator+(Vector2 p_vector) const;
+        Vector2 operator+(float p_val) const;
+
+        Vector2 operator-(Vector2 p_vector) const;
+        Vector2 operator-(float p_val) const;
+
+        Vector2 operator*(Vector2 p_vector) const;
+        Vector2 operator*(float p_val) const;
+
+        Vector2 operator/(Vector2 p_vector) const;
+        Vector2 operator/(float p_val) const;
+
+        bool operator==(Vector2 p_vector) const;
+        bool operator!=(Vector2 p_vector) const;
+
+        void operator+=(Vector2 p_vector);
+        void operator+=(float p_val);
+
+        void operator-=(Vector2 p_vector);
+        void operator-=(float p_val);
+
+        void operator*=(Vector2 p_vector);
+        void operator*=(float p_val);
+
+        void operator/=(Vector2 p_vector);
+        void operator/=(float p_val);
+
+    };
 }
